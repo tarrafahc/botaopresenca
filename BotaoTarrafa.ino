dae4f2b7a7d3705f9fb48344825df192580f8345
daniel <daniel@danielandrade.net>
2013-08-08 09:47:20 -0300

    Botao funcionando
    Versao atual, adicionado cinco minuto de luz branca quando desligado
    para segurança.

--- BotaoTarrafa.ino
+++ BotaoTarrafa.ino
@@ -1,16 +1,28 @@
-//Botao Tarrafa
+/*
+  Botao Tarrafa - Presenca
+  www.Tarrafa.net
+*/
 
 #include <EEPROM.h>
 
-// Pinos
-#define memory 0
-#define bPin 2
-#define led A0
+#define memory 0 // Memoria
+#define bPin 2   // Pino do botao
+#define led A0   // Led do botao
+#define fade(x,y) if (x>y) x--; else if (x<y) x++;
 
 int leds[] = {  10, 11, 9  };   // Usando os pinos VERMELHO - VERDE - AZUL
 
 static int valor;
 int desligando = 0;
+int time;
+
+//Random Color Generator Vars
+int redNow;
+int blueNow;
+int greenNow;
+int redNew;
+int blueNew;
+int greenNew;
 
 void setup() {
   //Leds
@@ -26,17 +38,52 @@ void setup() {
   
   //Interrupt for the button, 0 = Digital Pin 2
   attachInterrupt(0, butaoPress, FALLING);
+    
+ //Random Color Start Seed
+ redNow = random(255);    redNew = redNow;
+ blueNow = random(255);   blueNew = blueNow;
+ greenNow = random(255);  greenNew = greenNow;
 }
 
 void loop(){
+  //Ligado, vamos brincar com leds?
   if(valor==1){
-      digitalWrite(led,HIGH);
+    digitalWrite(led,HIGH);  
+    
+    //------------[Random Color Generator Tabajara]-----------------\\
+    
+     analogWrite(leds[2], blueNow);
+     analogWrite(leds[0], redNow);
+     analogWrite(leds[1], greenNow);
+     
+     redNew = random(255);
+     blueNew = random(255);
+     greenNew = random(255);
+    
+     // fade to new colors
+     while ((redNow != redNew) ||  (blueNow != blueNew) ||  (greenNow != greenNew)){
+      fade(redNow,redNew)
+      fade(blueNow,blueNew)
+      fade(greenNow,greenNew)
+      analogWrite(leds[2], blueNow);
+      analogWrite(leds[0], redNow);
+      analogWrite(leds[1], greenNow);
+      delay(20);
+     }
+    
+    //-----------------------------------------------------------------\\
+    
   }
   
   //Desliga tudo
   else{
-        for(int i = 0; i < 3; i++){digitalWrite(leds[i], LOW);}
-              digitalWrite(led,LOW);
+    
+    //Desliga LED Botao
+    digitalWrite(led,LOW);
+    //Se for mais do que 5mins, ele desliga a luz branca
+    if(millis() > time + 300000){  for(int i = 0; i < 3; i++){ digitalWrite(leds[i], LOW); }}
+    else{ for(int i = 0; i < 3; i++){ digitalWrite(leds[i], HIGH); }}
+
   }
 }
 
@@ -50,8 +97,11 @@ void butaoPress(){
     valor = !valor;
     EEPROM.write(memory, valor);
     Serial.println(valor,DEC);
+    time = millis();
   }
   last_interrupt_time = interrupt_time;
 }
+
+
 
 
